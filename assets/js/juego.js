48cce28bbd86698973a1b25e56635d90d39c7d22
Alexander Gomez <mpalexanderg@gmail.com>
2025-05-15 22:09:36 -0500

    Se agrego un module store y se agrego juego-min para producciòn

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,143 +1,140 @@
-/**
- * 2C = Two of Clubs (Tréboles)
- * 2D = Two of Diamonds (Diamantes)
- * 2H = Two of Hearts (Corazones)
- * 2S = Two of Spades (Espadas)
- */
+(() => {
+  "use strict";
+  /**
+   * 2C = Two of Clubs (Tréboles)
+   * 2D = Two of Diamonds (Diamantes)
+   * 2H = Two of Hearts (Corazones)
+   * 2S = Two of Spades (Espadas)
+   */
 
-let deck = [];
-let tipos = ['C', 'D', 'H', 'S'];
-let especiales = ['A', 'J', 'Q', 'K'];
-let puntosJugador = 0;
-let puntosComputadora = 0;
+  let   deck = [],
+        tipos = ["C", "D", "H", "S"],
+        especiales = ["A", "J", "Q", "K"],
+        puntosJugador = 0,
+        puntosComputadora = 0;
 
-// Referencias HTML
-const btnNuevo = document.querySelector('#btnNuevo');
-const btnPedir = document.querySelector('#btnPedir');
-const btnDetener = document.querySelector('#btnDetener');
-const divPuntosJugador = document.querySelector('#puntosJugador');
-const divPuntosComputadora = document.querySelector('#puntosComputadora');
-const divCartasJugador = document.querySelector('#jugador-cartas');
-const divCartasComputadora = document.querySelector('#computadora-cartas');
-// Referencias HTML Modal
-const btnModalNuevo = document.getElementById('btnNuevoJuegoModal');
-const btnCerrarModal = document.getElementById('btnCerrarModal');
-const mensajeResultado = document.getElementById('mensajeResultado');
-const modal = document.getElementById('resultadoModal');
-const puntajeFinalJugador = document.getElementById('puntajeFinalJugador');
-const puntajeFinalComputadora = document.getElementById('puntajeFinalComputadora');
+  // Referencias HTML
+  const btnNuevo = document.querySelector("#btnNuevo"),
+        btnPedir = document.querySelector("#btnPedir"),
+        btnDetener = document.querySelector("#btnDetener"),
+        divPuntosJugador = document.querySelector("#puntosJugador"),
+        divPuntosComputadora = document.querySelector("#puntosComputadora"),
+        divCartasJugador = document.querySelector("#jugador-cartas"),
+        divCartasComputadora = document.querySelector("#computadora-cartas"),
+  // Referencias HTML Modal
+        btnModalNuevo = document.getElementById("btnNuevoJuegoModal"),
+        btnCerrarModal = document.getElementById("btnCerrarModal"),
+        mensajeResultado = document.getElementById("mensajeResultado"),
+        modal = document.getElementById("resultadoModal"),
+        puntajeFinalJugador = document.getElementById("puntajeFinalJugador"),
+        puntajeFinalComputadora = document.getElementById("puntajeFinalComputadora");
 
-/**
- * Crea un nuevo deck (Baraja)
- * @returns Retorna un nuevo deck (Baraja)
- */
-const crearDeck = () => {
+  /**
+   * Crea un nuevo deck (Baraja)
+   * @returns Retorna un nuevo deck (Baraja)
+   */
+  const crearDeck = () => {
     // Creamos cartas del 2 al 10
     for (let i = 2; i <= 10; i++) {
-        for (let tipo of tipos) {
-            deck.push(i + tipo);
-        }
+      for (let tipo of tipos) {
+        deck.push(i + tipo);
+      }
     }
     // Creamos cartas especiales
     for (let tipo of tipos) {
-        for (let esp of especiales) {
-            deck.push(esp + tipo);
-        }
+      for (let esp of especiales) {
+        deck.push(esp + tipo);
+      }
     }
-    // Mezclamos el deck
-    deck = _.shuffle(deck);
-    return deck;
-}
-crearDeck();
-/**
- * Pide una carta de la baraja
- * @returns Retorna una carta
- */
-const pedirCarta = () =>{
+    // Retornamos el deck mezclado
+    return _.shuffle(deck);;
+  };
+  crearDeck();
+  /**
+   * Pide una carta de la baraja
+   * @returns Retorna una carta
+   */
+  const pedirCarta = () => {
     // Validar que haya cartas en el deck
     if (deck.length === 0) {
-        return 'No hay cartas en el deck';
+      return "No hay cartas en el deck";
     }
-    // Sacar la última carta del deck
-    const carta = deck.pop();
-    return carta;
-}
-/**
- * Obtiene el valor de la carta
- * @param {string} carta Carta a obtener el valor
- * @returns Retorna el valor de la carta
- */
-const valorCarta = (carta) => {
-    const valor = carta.substring(0, carta.length -1);
-    return  (isNaN(valor)) ?
-            (valor === 'A') ? 11 : 10
-            : valor * 1;
-}
+    // Retornamos última carta del deck
+    return deck.pop();
+  };
+  /**
+   * Obtiene el valor de la carta
+   * @param {string} carta Carta a obtener el valor
+   * @returns Retorna el valor de la carta
+   */
+  const valorCarta = (carta) => {
+    const valor = carta.substring(0, carta.length - 1);
+    return isNaN(valor) ? (valor === "A" ? 11 : 10) : valor * 1;
+  };
 
-/**
- * Muestra el resultado del juego
- */
-const mostrarResultado = () => {
+  /**
+   * Muestra el resultado del juego
+   */
+  const mostrarResultado = () => {
     puntajeFinalJugador.textContent = puntosJugador;
     puntajeFinalComputadora.textContent = puntosComputadora;
 
-    if (puntosComputadora === puntosJugador){
-        mensajeResultado.textContent = 'Nadie gana';
-        modal.removeAttribute('hidden');
-    } else if (puntosJugador > 21){
-        mensajeResultado.textContent = 'Computadora gana';
-        modal.removeAttribute('hidden');
-    } else if (puntosComputadora > 21){
-        mensajeResultado.textContent = 'Jugador gana';
-        modal.removeAttribute('hidden');
-    } else if (puntosComputadora > puntosJugador){
-        mensajeResultado.textContent = 'Computadora gana';
-        modal.removeAttribute('hidden');
+    if (puntosComputadora === puntosJugador) {
+      mensajeResultado.textContent = "Nadie gana";
+      modal.removeAttribute("hidden");
+    } else if (puntosJugador > 21) {
+      mensajeResultado.textContent = "Computadora gana";
+      modal.removeAttribute("hidden");
+    } else if (puntosComputadora > 21) {
+      mensajeResultado.textContent = "Jugador gana";
+      modal.removeAttribute("hidden");
+    } else if (puntosComputadora > puntosJugador) {
+      mensajeResultado.textContent = "Computadora gana";
+      modal.removeAttribute("hidden");
     }
-}
+  };
 
-/**
- * Turno de la computadora
- * @param {number} puntosJugador Puntos del jugador
- * @returns Puntos de la computadora
- */
-const turnoComputadora = (puntosJugador) => {
+  /**
+   * Turno de la computadora
+   * @param {number} puntosJugador Puntos del jugador
+   * @returns Puntos de la computadora
+   */
+  const turnoComputadora = (puntosJugador) => {
     do {
-        // Pido la carta
-        const carta = pedirCarta();
-        // Obtengo el valor de la carta
-        const valor = valorCarta(carta);
-        // Sumo el valor a los puntos de la computadora
-        puntosComputadora += valor;
-        divPuntosComputadora.innerText = puntosComputadora;
-        // Muestro la carta en el HTML
-        const imgCarta = document.createElement('img');
-        imgCarta.src = `./assets/cartas/${carta}.png`;
-        imgCarta.classList.add('carta');
-        divCartasComputadora.append(imgCarta);
-        mostrarResultado();
+      // Pido la carta
+      const carta = pedirCarta();
+      // Obtengo el valor de la carta
+      const valor = valorCarta(carta);
+      // Sumo el valor a los puntos de la computadora
+      puntosComputadora += valor;
+      divPuntosComputadora.innerText = puntosComputadora;
+      // Muestro la carta en el HTML
+      const imgCarta = document.createElement("img");
+      imgCarta.src = `./assets/cartas/${carta}.png`;
+      imgCarta.classList.add("carta");
+      divCartasComputadora.append(imgCarta);
+      mostrarResultado();
+    } while (puntosComputadora < puntosJugador && puntosJugador <= 21);
+  };
 
-    } while(puntosComputadora < puntosJugador && (puntosJugador <= 21));
-}
-
-/**
- * Reinicia el juego
- */
-const nuevoJuego = () =>{
+  /**
+   * Reinicia el juego
+   */
+  const nuevoJuego = () => {
     deck = [];
     puntosJugador = 0;
     puntosComputadora = 0;
-    divCartasJugador.innerHTML = '';
-    divCartasComputadora.innerHTML = '';
+    divCartasJugador.innerHTML = "";
+    divCartasComputadora.innerHTML = "";
     divPuntosJugador.innerText = 0;
     divPuntosComputadora.innerText = 0;
     btnPedir.disabled = false;
     btnDetener.disabled = false;
     crearDeck();
-}
+  };
 
-// Eventos
-btnPedir.addEventListener('click', () => {
+  // Eventos
+  btnPedir.addEventListener("click", () => {
     // Pido la carta
     const carta = pedirCarta();
     // Obtengo el valor de la carta
@@ -147,38 +144,39 @@ btnPedir.addEventListener('click', () => {
     // Muestro el valor en el HTML
     divPuntosJugador.innerText = puntosJugador;
     // Muestro la carta en el HTML
-    const imgCarta = document.createElement('img');
+    const imgCarta = document.createElement("img");
     imgCarta.src = `./assets/cartas/${carta}.png`;
-    imgCarta.classList.add('carta');
+    imgCarta.classList.add("carta");
     divCartasJugador.append(imgCarta);
 
     // Validar si el jugador perdió
-    if(puntosJugador > 21){
-        btnPedir.disabled = true;
-        btnDetener.disabled = true;
-        turnoComputadora(puntosJugador);
+    if (puntosJugador > 21) {
+      btnPedir.disabled = true;
+      btnDetener.disabled = true;
+      turnoComputadora(puntosJugador);
     } else if (puntosJugador === 21) {
-        btnPedir.disabled = true;
-        btnDetener.disabled = true;
-        turnoComputadora(puntosJugador);
+      btnPedir.disabled = true;
+      btnDetener.disabled = true;
+      turnoComputadora(puntosJugador);
     }
-});
+  });
 
-btnDetener.addEventListener('click', () => {
+  btnDetener.addEventListener("click", () => {
     btnPedir.disabled = true;
     btnDetener.disabled = true;
     turnoComputadora(puntosJugador);
-});
+  });
 
-btnNuevo.addEventListener('click', () => {
+  btnNuevo.addEventListener("click", () => {
     nuevoJuego();
-});
+  });
 
-btnModalNuevo.addEventListener('click', function() {
+  btnModalNuevo.addEventListener("click", function () {
     nuevoJuego();
-    modal.setAttribute('hidden', true);
-});
+    modal.setAttribute("hidden", true);
+  });
 
-btnCerrarModal.addEventListener('click', function() {
-    modal.setAttribute('hidden', true);
-});
+  btnCerrarModal.addEventListener("click", function () {
+    modal.setAttribute("hidden", true);
+  });
+})();
